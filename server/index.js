2cbf76771d35af0a6e2e83eef50cb26f116e59bb
Meet Shah <shahmeet8210@gmail.com>
2026-04-09 14:31:36 -0400

    Database Connection Error Fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,17 @@ import app from "../api/index.js";
 
 const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`[api] listening on http://127.0.0.1:${PORT}`);
+});
+
+server.on("error", (err) => {
+  if (err && typeof err === "object" && err.code === "EADDRINUSE") {
+    console.error(
+      `[api] port ${PORT} is already in use. Stop the process using it, or run with PORT=<free_port>.`
+    );
+    process.exit(1);
+  }
+  throw err;
 });
 
